dfffaf61ed769ce0476df3d07ad2c1fb8f8f0a4e
Manish <official.iamanish@gmail.com>
2022-05-26 14:12:45 +0530

    first change

--- README.md
+++
@@ -1,2 +0,0 @@
-# Data-Structure-Algorithms
-Uploading Solutions of any coding Platform in designated folder.
--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
     cout<<"Manish Kumar";
+    cout<<"#30 days of coding challenge";
     return 0;
 }
