d5cdba89a12bb705d81edddd5d64ea47f35fcb5f
Manish <official.iamanish@gmail.com>
2022-06-09 22:47:43 +0530

    Code

---
+++ leetcode/1332.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+// 1332. Remove Palindromic Subsequences
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        
+        int i = 0, j = s.size() - 1;
+         
+        while(i < j) 
+        {
+            if(s[i] != s[j]) 
+                return 2;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+};
