5f523238884ffec63f5bd89a57eb912d40bef314
Manish <official.iamanish@gmail.com>
2022-07-08 13:34:38 +0530

    Code

---
+++ DSA 450/array/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProf = 0, minNum = INT_MAX;
+        for(int i = 0 ; i < prices.size() ; i++) {
+            if(prices[i] < minNum)
+                minNum = prices[i];
+            if(prices[i] - minNum > maxProf)
+                maxProf = prices[i] - minNum;
+        }
+        return maxProf;
+    }
+};
