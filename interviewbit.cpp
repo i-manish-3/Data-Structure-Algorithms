c38087bd8ca7f0073315f3f68d12eae6d775dd54
Manish <official.iamanish@gmail.com>
2022-07-09 18:59:01 -1200

    Code

--- interviewbit.cpp
+++ interviewbit.cpp
@@ -26,26 +26,61 @@
 
 
 
+// #include<bits/stdc++.h> 
+// using namespace std;
+
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--)
+//     {
+//         int n,x;
+//         cin>>n>>x;
+//         int cost = 0;
+//         if(n%6 == 0)
+//         {
+//             cost = x * (n/6);
+//             cout<<cost<<endl;
+//         }
+//         else{
+//             cost = x * ((n/6)+1);
+//             cout<<cost<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h> 
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--)
+int solve(vector<int> &arr, int k)
+{
+    vector<int> res;
+    set<vector<int>> s;
+    map<int,int> mp;
+    for(int i=0; i<arr.size(); i++)
     {
-        int n,x;
-        cin>>n>>x;
-        int cost = 0;
-        if(n%6 == 0)
-        {
-            cost = x * (n/6);
-            cout<<cost<<endl;
-        }
-        else{
-            cost = x * ((n/6)+1);
-            cout<<cost<<endl;
-        }
+        mp[arr[i]]++;
     }
+    
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(mp.find(arr[i] + k) != mp.end())
+        {
+            res.push_back(arr[i]);
+            res.push_back(arr[i]+k);
+            s.insert(res);
+            res.clear();
+        }
+        
+    }
+    return s.size();
+}
+int main(){
+    vector<int> arr{1,1,2,2,3,3};
+    int k = 1;
+    int ans = solve(arr,k);
+    cout<<ans; 
     return 0;
 }
+
