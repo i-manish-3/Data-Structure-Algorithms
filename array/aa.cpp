9ebec2360b63e09e5c506c6182bd808094a91482
Manish <official.iamanish@gmail.com>
2022-07-18 13:56:22 +0530

    Code

--- array/aa.cpp
+++ array/aa.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 // 4 98 2 6 44 55
 
-int stocks(vector<int> arr)
-{
-    int maxProfit = 0;
-    int finalProfit = 0;
-    int start = arr[0];
+// int stocks(vector<int> arr)
+// {
+//     int maxProfit = 0;
+//     int finalProfit = 0;
+//     int start = arr[0];
 
-    for(int i = 1; i < arr.size(); i++)
-    {
-        start = min(start, arr[i]);
-        int profit = arr[i] - start;
-        maxProfit = max(profit, maxProfit);
-        finalProfit += maxProfit;
-    }
+//     for(int i = 1; i < arr.size(); i++)
+//     {
+//         start = min(start, arr[i]);
+//         int profit = arr[i] - start;
+//         maxProfit = max(profit, maxProfit);
+//         finalProfit += maxProfit;
+//     }
 
-}
+// }
 
 int main(){
     
